c21c5db016eeea1c3a2afa0b58f6f449332a8f80
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-09-09 14:53:03 +0530

    commit changes

--- ToDoAPI.cs
+++ ToDoAPI.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Azure.Functions.Worker;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace EdSmart
@@ -14,7 +16,7 @@ namespace EdSmart
     {
         private static List<Todo> items = new();
 
-        [Function("CreateTodo")]
+        [FunctionName("CreateTodo")]
         public static async Task<IActionResult> CreateTodo(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")]
             HttpRequest req,
@@ -24,11 +26,62 @@ namespace EdSmart
             log.LogInformation("Creating a new todo list item");
 
             var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var input = JsonConvert.DeserializeObject<ToDoCreateModel>(requestBody);
+            var input = JsonConvert.DeserializeObject<CreateToDoModel>(requestBody);
 
             var todo = new Todo() { TaskDescription = input.TaskDescription, IsCompleted = false };
             items.Add(todo);
             return new OkObjectResult(todo);
+        }
+
+        [FunctionName("GetTodos")]
+        public static async Task<IActionResult> GetTodos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")]
+            HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Getting todo list items");
+            return new OkObjectResult(items);
+        }
+
+        [FunctionName("GetTodoById")]
+        public static async Task<IActionResult> GetTodoById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")]
+            HttpRequest req,
+            ILogger log, string id)
+        {
+            log.LogInformation("Getting todo list item by Id : {id}");
+            var todo = items.FirstOrDefault(c => c.Id == id);
+            return todo == null ? new NotFoundResult() : new OkObjectResult(todo);
+        }
+
+        [FunctionName("UpdateTodo")]
+        public static async Task<IActionResult> UpdateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")]
+            HttpRequest req,
+            ILogger log, string id)
+        {
+            log.LogInformation("Update to list item by Id : {0}", id);
+            var todo = items.FirstOrDefault(c => c.Id == id);
+            if (todo == null) return new NotFoundResult();
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var updated = JsonConvert.DeserializeObject<TodoUpdateModel>(requestBody);
+            todo.IsCompleted = updated.IsCompleted;
+            if (!string.IsNullOrEmpty(updated.TaskDescription)) todo.TaskDescription = updated.TaskDescription;
+            return new OkObjectResult(todo);
+        }
+
+
+        [FunctionName("DeleteTodo")]
+        public static async Task<IActionResult> DeleteTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")]
+            HttpRequest req,
+            ILogger log, string id)
+        {
+            log.LogInformation("Update to list item by Id : {0}", id);
+            var todo = items.FirstOrDefault(c => c.Id == id);
+            if (todo == null) return new NotFoundResult();
+            items.Remove(todo);
+            return new OkResult();
         }
     }
 }
--- ToDoModel.cs
+++ ToDoModel.cs
@@ -1,14 +1,21 @@
 using System;
 
-public class ToDoCreateModel
-{
-    public string TaskDescription { get; set; }
-}
 
 public class Todo
 {
     public string Id { get; set; } = new Guid().ToString("n");
     public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+    public string TaskDescription { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+public class CreateToDoModel
+{
+    public string TaskDescription { get; set; }
+}
+
+public class TodoUpdateModel
+{
     public string TaskDescription { get; set; }
     public bool IsCompleted { get; set; }
 }
